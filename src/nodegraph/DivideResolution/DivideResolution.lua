9df3231a4383892e5a6cace480a9ee4ca8bbe18d
Liam Collod <lcollod@gmail.com>
2021-10-19 12:43:42 +0200

    created DivideResolution

---
+++ src/nodegraph/DivideResolution/DivideResolution.lua
@@ -0,0 +1,76 @@
+--[[
+VERSION = 0.0.5
+
+OpScript for Foundry's Katana software
+
+Divide the current render resolution by the given amount.
+The divider amount can be supplied or by creating an OpArg (user.divider) or by creating a gsv "resolution_divider".
+
+Author: Liam Collod
+Last Modified: 19/10/2021
+
+[OpScript setup]
+parameters:
+    location: /root
+    applyWhere: at specific location
+user:
+    divider: (int)
+]]
+
+
+
+local round = function(a, prec)
+    -- source: https://stackoverflow.com/questions/9654496/lua-converting-from-float-to-int
+    return math.floor(a + 0.5*prec) -- where prec is 10^n, starting at 0
+end
+
+
+function get_divider(frame)
+    --[[
+    Return the resolution divider from a graphstatevariable named 'resolution_divider' if it exists, else from the OpArg user.divider
+
+    Args:
+        frame(int): current frame
+    Returns:
+        int: resolution divider
+    ]]
+    local gsv_value = Interface.GetGraphStateVariable("resolution_divider")
+    if gsv_value then
+       return tonumber(gsv_value:getValue())
+    end
+
+    local argvalue = Interface.GetOpArg("user.divider")
+    if argvalue then
+        return argvalue:getNearestSample(frame)[1]
+    end
+    
+end
+
+
+function run()
+
+    local frame = Interface.GetCurrentTime() -- int
+    local divider = get_divider(frame)
+
+    -- divider == 0 or 1 means we doesn't want to apply any resolution reformating.
+    if divider == 0 or divider==1 then
+        return
+    end
+
+    local resolution = Interface.GetAttr("renderSettings.resolution"):getNearestSample(frame)[1]
+    resolution = ResolutionTable.GetResolution(resolution)
+    if not resolution then
+        print("[DivideResolution][run] renderSettings.resolution has an issue.  ResolutionTable.GetResolution() returned nil.")
+        return
+    end
+
+    local new_resolution_x = round(resolution:getXRes() / divider, 0)
+    local new_resolution_y = round(resolution:getYRes() / divider, 0)
+    local new_resolution =  tostring(new_resolution_x) .. "x" .. tostring(new_resolution_y) -- str
+
+    Interface.SetAttr("renderSettings.resolution", StringAttribute(new_resolution))
+
+end
+
+-- execute
+run()
---
+++ src/nodegraph/DivideResolution/README.md
@@ -0,0 +1,31 @@
+# Divide Resolution
+
+![OpScript](https://img.shields.io/badge/type-OpScript-blueviolet)
+
+Divide the current render resolution by the given amount.
+
+![demo](./demo.gif)
+
+
+
+## Installation
+
+### .lua
+
+Create a new OpScript node and copy/paste the content of the .lua script inside.
+Follow the instructions on the top comment to setup the node.
+
+
+
+## Guide
+
+The divider can be configured in two ways:
+
+### OpArg
+
+You can create an `user.divider` number parameter on the OpScript node.
+
+### GSV
+
+You can set it using a variable named `resolution_divider`. This one take priority over the above one.
+
