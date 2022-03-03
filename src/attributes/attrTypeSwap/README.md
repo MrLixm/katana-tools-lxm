5504c9b90c493631cb16d606cca03eeb0e68ec3d
Liam Collod <lcollod@gmail.com>
2022-03-03 17:36:20 +0100

    attrTypeSwap: first commit

---
+++ src/attributes/attrTypeSwap/README.md
@@ -0,0 +1,46 @@
+# attrTypeSwap
+
+Quickly change the data type used for an attribute for another one.
+
+# Use
+
+## Installation
+
+You can use the [createOpScript.py](createOpScript.py) file to quickly 
+install the script, ready for use.
+
+> ❕ check that the SCRIPT variable at the top of the file use the latest version
+> of the [attrTypeSwap.lua](attrTypeSwap.lua) script.
+
+To use it copy-paste the content of [createOpScript.py](createOpScript.py)
+in the Python tab and execute it.
+
+An OpScript node named `OpScript_attrTypeSwap_1` should be created at the top-left 
+of the nodegraph.
+
+## Config
+
+First set the scene-graph location where you want to modify some of its attributes
+in the `location` parameter at top.
+
+You can then modify the `user.attributes` parameter to add attribute to modify.
+Each row corresponds to an attribute to modify where :
+
+- column [1*n] = path of the attribute relative to the location set 
+- column [2*n] = new DataAttribute type to use, ex: StringAttribute
+
+Exemple : 
+
+![opscript interface screenshot](img.png)
+
+List of DataAttribute can be found on
+[the Katana documentation](https://learn.foundry.com/katana/5.0/dev-guide/OpsAndOpScript/Attributes/OpScript.html#IntAttribute)
+but the script supports :
+
+```
+IntAttribute,
+FloatAttribute
+DoubleAttribute
+StringAttribute
+```
+
---
+++ src/attributes/attrTypeSwap/createOpScript.py
@@ -0,0 +1,191 @@
+"""
+version=1
+
+Create the OpScript node for the attTypeSwap script.
+Check that SCRIPT variable use the latest version of the .lua file that should
+be shipped alongside this script.
+"""
+from Katana import NodegraphAPI, UI4
+
+FRAME = NodegraphAPI.GetCurrentTime()
+
+SCRIPT = """
+--[[
+version=3
+author=Liam Collod
+last_modified=03/03/2022
+
+[OpScript setup]
+parameters:
+  location: location where attribute must be modified
+  applyWhere: at specific location
+user:
+  attributes(string array): array of string where:
+    - [1*n] = path of the attribute relative to the location
+    - [2*n] = new DataAttribute type to use, ex: StringAttribute
+]]
+
+local function get_user_attr(time, name, default_value)
+    --[[
+    Return an OpScipt user attribute.
+    If not found return the default_value. (unless asked to raise an error)
+
+    Args:
+        time(int): frame the attribute must be queried at
+        name(str): attribute location (don't need the <user.>)
+        default_value(any): value to return if user attr not found
+          you can use the special token <$error> to raise an error instead
+    Returns:
+        table or any: table of value on attribute or default value
+    ]]
+    local argvalue = Interface.GetOpArg("user."..name)
+
+    if argvalue then
+      return argvalue:getNearestSample(time)
+
+    elseif default_value=="$error" then
+      error("[get_user_attr] user attribute <",name,"> not found.")
+
+    else
+      return default_value
+
+    end
+
+end
+
+local function get_loc_attr(attr_path, time, default)
+  --[[
+  Get the given attribute on the location at given time.
+  Raise an error is nil result is found or return <default> if specified.
+
+  If default is not nil and the attribute is not found, it is instead returned.
+
+  Args:
+    attr_path(str): path of the attribute on the location
+    time(int): frame to extract the value from
+    default(any or nil): value to return if attribute not found.
+  Returns:
+    table, num: [1]:table of value, [2] tuple size for the attribute
+  ]]
+
+  local lattr = Interface.GetAttr(attr_path)
+
+  if not lattr then
+
+    if default ~= nil then
+      return default
+    end
+
+    error(
+      "[get_loc_attr] Attr <",attr_path,"> not found on source <",location,">."
+    )
+
+  end
+
+  local lattr_tuple = lattr:getTupleSize()
+  lattr = lattr:getNearestSample(time)
+
+  if not lattr then
+
+    if default ~= nil then
+      return default
+    end
+
+    error(
+      "[get_loc_attr] Attr <", attr_path, "> is nil on source <", location,
+      "> at time=", time
+    )
+
+  end
+
+  return lattr, lattr_tuple
+
+end
+
+local function get_attribute_class(class_name)
+  --[[
+  Returned a non-instanced version of the DataAttribute given in arg.
+
+  Args:
+    class_name(str): name of the DataAttribute class
+  Returns:
+    table: DataAttribute
+  ]]
+  if class_name == "IntAttribute" then
+    return IntAttribute
+  elseif class_name == "FloatAttribute" then
+    return FloatAttribute
+  elseif class_name == "DoubleAttribute" then
+    return DoubleAttribute
+  elseif class_name == "StringAttribute" then
+    return StringAttribute
+  else
+    error(
+      "[get_attribute_class] passed class name <",
+      class_name,
+      ">is not supported."
+    )
+  end
+end
+
+
+local function run()
+
+  local time = Interface.GetCurrentTime()
+
+  local attr_list = get_user_attr(time, "attributes", "$error")
+
+  local attr local value local attr_type local tuple_size
+  for i=0, #attr_list / 2 - 1 do
+
+    attr = attr_list[i*2+1]
+    attr_type = get_attribute_class(attr_list[i*2+2])
+
+    value, tuple_size = get_loc_attr(attr, time, nil)
+    Interface.SetAttr(attr, attr_type(value, tuple_size))
+
+  end
+
+end
+
+run()
+"""
+
+
+def get_node_screen_pos(x_offset=0, y_offset=0):
+    """
+    Return the position from the top left corner of the nodegraph
+
+    Args:
+        x_offset:
+        y_offset:
+
+    Returns:
+        tuple:
+
+    """
+
+    nodegraph_tab = UI4.App.Tabs.FindTopTab('Node Graph')
+    nodegraph_widget = nodegraph_tab.getNodeGraphWidget()
+    view_dimensions = nodegraph_widget.getVisibleArea()  # type: tuple
+
+    return view_dimensions[0][0] + x_offset, view_dimensions[1][1] + y_offset
+
+
+node = NodegraphAPI.CreateNode(
+    "OpScript",
+    NodegraphAPI.GetRootNode()
+)
+node.setName("OpScript_attrTypeSwap_1")
+
+pos = get_node_screen_pos(350, -350)
+NodegraphAPI.SetNodePosition(node, pos)
+
+node.getParameter("applyWhere").setValue("at specific location", FRAME)
+node.getParameter("script.lua").setValue(SCRIPT, FRAME)
+userparam = node.getParameters().createChildGroup('user')
+p = userparam.createChildStringArray("attributes", 2)
+p.setHintString(repr({"resize": True, "tupleSize": 2}))
+
+NodegraphAPI.SetAllSelectedNodes([node])
+NodegraphAPI.SetNodeEdited(node, True, exclusive=True)
