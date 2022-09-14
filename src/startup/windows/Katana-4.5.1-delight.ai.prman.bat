423bb336c40460a1e95786b46452c34d8220fb54
Liam Collod <lcollod@gmail.com>
2022-09-14 20:49:13 +0200

    [startup] fixed startup script

--- src/startup/windows/Katana-4.5.1-delight.ai.prman.bat
+++ src/startup/windows/Katana-4.5.1-delight.ai.prman.bat
@@ -13,16 +13,15 @@ set "KATANA_TAGLINE=With 3Delight, Arnold 4.0.0.2, RenderMan 24.3, XGEN"
 set KATANA_CATALOG_RECT_UPDATE_BUFFER_SIZE=1
 set "KATANA_USER_RESOURCE_DIRECTORY=%U_USER_PREFS%"
 
-rem optional
 set "PATH=%PATH%;%U_KATANA_HOME%\bin";
 rem for opscripting :
-set "LUA_PATH=%LUA_PATH%;whatever/?.lua"
+set "LUA_PATH=%LUA_PATH%;whatever\?.lua"
 
 :: Delight config
 set "U_DELIGHT=C:\Program Files\3Delight"
-set "DLFK_INSTALL_PATH=C:\Program Files\3Delight\3DelightForKatana"
-set "DL_DISPLAYS_PATH=C:\Program Files\3Delight\displays"
-set "DL_SHADERS_PATH=C:\Program Files\3Delight\shaders"
+set "DLFK_INSTALL_PATH=%U_DELIGHT%\3DelightForKatana"
+set "DL_DISPLAYS_PATH=%U_DELIGHT%\displays"
+set "DL_SHADERS_PATH=%U_DELIGHT%\shaders"
 set "DLC_KATANA=%DLFK_INSTALL_PATH%\Plugins"
 set "PATH=%PATH%;%U_DELIGHT%\bin"
 set "KATANA_RESOURCES=%KATANA_RESOURCES%;%DLFK_INSTALL_PATH%"
@@ -41,8 +40,8 @@ set "RMANTREE=C:\Program Files\Pixar\RenderManProServer-24.3"
 set "KATANA_RESOURCES=%KATANA_RESOURCES%;%U_PRMAN_HOME%\plugins\%U_PRMAN_KATANA_VERSION%"
 
 :: Xgen configuration
-set "MAYA_INSTALLATION=C:\Program Files\Autodesk\Maya2020"
-set "XGEN_LOCATION=%MAYA_INSTALLATION%\plug-ins\xgen"
-set "PATH=%PATH%;%XGEN_LOCATION%;%XGEN_LOCATION%\bin;%MAYA_INSTALLATION%\bin;"
+set "U_MAYA_INSTALLATION=C:\Program Files\Autodesk\Maya2020"
+set "U_XGEN_LOCATION=%U_MAYA_INSTALLATION%\plug-ins\xgen"
+set "PATH=%PATH%;%U_XGEN_LOCATION%;%U_XGEN_LOCATION%\bin;%U_MAYA_INSTALLATION%\bin;"
 
 start "" "%U_KATANA_HOME%\bin\katanaBin.exe"
